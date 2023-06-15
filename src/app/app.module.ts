e78fd1cc81d95d49be2269b355bfb6f8b1d2e548
kl1mmm <klimovilya07@mail.ru>
2023-06-15 17:01:00 +0300

    Router are created and configured.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,23 @@ import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
 import {NamePageComponent} from './name-page/name-page.component';
 import {CountriesComponent} from './countries/countries.component';
+import {ErrorPageComponent} from './error-page/error-page.component';
+import {HomeComponent} from './home/home.component';
 
 const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
   {path: 'name', component: NamePageComponent},
   {path: 'countries', component: CountriesComponent},
+  {path: '**', component: ErrorPageComponent}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     NamePageComponent,
-    CountriesComponent
+    CountriesComponent,
+    ErrorPageComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
